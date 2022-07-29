bef29b9dc682419c64bf40a1822fa1dabdf93551
Elliot Alderson <110250872+HotFixDeveloper@users.noreply.github.com>
2022-07-29 17:55:41 +0000

    fix txt file name

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 # простая функция проверки текстового файла на наличие слов "vpn"
-File.open('domens.txt', 'r') do |file|
+File.open('sites.txt', 'r') do |file|
   puts file.read().include? 'vpn'
 end
